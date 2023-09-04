ff14332d8e0d2180798997b67e58d739d1213b15
Rosario Scarzone <rscarzone@live.com>
2023-09-04 11:35:02 -0700

    updated openAppFeature branch

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,21 @@ public class Main
     {
 
         // Creates an array of Strings where each String is a URL of a website you want to open.
-//        String[] websites = {
-//                "https://csuglobal.instructure.com/",
-//                "https://www.udemy.com/",
-//                "https://chat.openai.com/"
-//        };
+        String[] websites = {
+                "https://csuglobal.instructure.com/",
+                "https://www.udemy.com/",
+                "https://chat.openai.com/"
+        };
 
         //This for-each loop iterates through each website URL stored in the websites array.
-//        for (String website : websites) {
-//            try {
-//                Desktop.getDesktop().browse(new URI(website));
-//                Thread.sleep(1000);
-//            } catch (IOException | URISyntaxException | InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
+        for (String website : websites) {
+            try {
+                Desktop.getDesktop().browse(new URI(website));
+                Thread.sleep(1000);
+            } catch (IOException | URISyntaxException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
         // Here, System.getProperty("os.name") fetches the name of the operating system as a String.
         // toLowerCase() then converts this String to all lowercase so that the subsequent comparisons
